e5b2913c04bb1b8e15aa85170284f8fe3a36280e
Msoffice12861@office365svip.tw <Msoffice12861@office365svip.tw>
2021-10-07 12:12:25 +0200

    commit

--- NegyedikHet_LKX2MO/NegyedikHet_LKX2MO/Form1.cs
+++ NegyedikHet_LKX2MO/NegyedikHet_LKX2MO/Form1.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace NegyedikHet_LKX2MO
 {
     public partial class Form1 : Form
     {
+        List<Flat> Flats;
+        RealEstateEntities1 context = new RealEstateEntities1();
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+        private void LoadData()
+        {
+           Flats=context.Flats.ToList();
         }
     }
 }
